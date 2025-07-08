8c0e4be591b9314483e3ba4a5ffef7e7b34d2887
infiniteuz <seriyafilm@gmail.com>
2025-07-09 01:17:10 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py kodi
+import os
+import logging
+from flask import Flask, request, jsonify
+from aiogram import Bot
+from bot import dp, setup_bot
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+
+# ... (to'liq kodni bu yerga joylang)
